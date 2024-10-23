07086f5e5f82a0e23d66af0310f8a34e9092c66f
projectorb <66729989+projectorb@users.noreply.github.com>
2024-10-23 23:19:32 +0800

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	skipTrailingSlashRedirect: true,
+	trailingSlash: true,
+	output : 'export',
+};
 
 export default nextConfig;
