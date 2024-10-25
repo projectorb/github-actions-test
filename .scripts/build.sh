e47cc7548cdb0c2deeb35939f88e37be7e8c8ebc
projectorb <66729989+projectorb@users.noreply.github.com>
2024-10-25 20:58:42 +0800

    add scripts

---
+++ .scripts/build.sh
@@ -0,0 +1,12 @@
+set -e
+
+echo "Building the project..."
+
+git pull origin main
+
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# nvm install
+
+# npm build
