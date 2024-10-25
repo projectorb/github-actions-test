5a6146b13b30591041a337c04947d7b4266cbdbb
projectorb <66729989+projectorb@users.noreply.github.com>
2024-10-25 21:27:02 +0800

    update again

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -7,8 +7,8 @@ git pull origin main
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-echo "Installing node version 22.2.0..."
-
 # nvm install
 
-# npm build
+npm build
+
+echo "Build complete."
